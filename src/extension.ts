131f62dfd01eeee93d6b1fade0df1702038ac48e
kaskamal <kasra.kamal@gmail.com>
2019-06-17 23:32:34 -0700

    Added StatusBarItem

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,102 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import {StatusBarItem, StatusBarAlignment, window, commands, ExtensionContext, Disposable} from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate({subscriptions}: ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "sumnum" is now active!');
+	// Print to console successfull activation of extension
+	console.log('Congratulations, your extension "sumnum" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	const commandId = "sample.showSelectionCount";
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+	let wordCounter = new WordCounter(commandId);
+	let wordCounterController = new WordCounterController(wordCounter);
+	subscriptions.push(wordCounter);
+	subscriptions.push(wordCounterController);
 
-	context.subscriptions.push(disposable);
+
+	subscriptions.push(commands.registerCommand(commandId, () => {
+		let n = wordCounter.updateWordCount();
+		window.showInformationMessage(`Yeah, ${n} number(s) selected`);
+	}));
+
+	// // The command has been defined in the package.json file
+	// // Now provide the implementation of the command with registerCommand
+	// // The commandId parameter must match the command field in package.json
+	// let disposable = commands.registerCommand('extension.helloWorld', () => {
+	// 	// The code you place here will be executed every time your command is executed
+
+	// 	// Display a message box to the user
+	// 	window.showInformationMessage('Hello World!');
+	// });
+
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+
+class WordCounter {
+	private statusBar: StatusBarItem;
+
+
+	constructor(commandId: string) {
+		this.statusBar = window.createStatusBarItem(StatusBarAlignment.Left);
+		this.statusBar.command = commandId;
+	}
+
+	public updateWordCount() {
+
+		// Get the current text editor
+		let editor = window.activeTextEditor;
+		// Hide status bar if not using text editor
+		if (!editor) {
+			this.statusBar.hide();
+			return;
+		}
+
+		// Retrieve text
+		let selection = editor.selection;
+		let text = editor.document.getText(selection);
+
+		let wordCount = this.extractWordCount(text);
+
+		this.statusBar.text = `Sum: ${wordCount}`;
+        this.statusBar.show();
+
+	}
+
+	public extractWordCount(text: string) {
+		return 100;
+	}
+
+	public dispose() {
+		this.statusBar.dispose();
+	}
+}
+
+
+
+class WordCounterController {
+
+    private _wordCounter: WordCounter;
+    private _disposable: Disposable;
+
+    constructor(wordCounter: WordCounter) {
+        this._wordCounter = wordCounter;
+
+        // subscribe to selection change and editor activation events
+        let subscriptions: Disposable[] = [];
+        window.onDidChangeTextEditorSelection(this._onEvent, this, subscriptions);
+        window.onDidChangeActiveTextEditor(this._onEvent, this, subscriptions);
+
+        // update the counter for the current file
+        this._wordCounter.updateWordCount();
+
+        // create a combined disposable from both event subscriptions
+        this._disposable = Disposable.from(...subscriptions);
+    }
+
+    dispose() {
+        this._disposable.dispose();
+    }
+
+    private _onEvent() {
+        this._wordCounter.updateWordCount();
+    }
+}
