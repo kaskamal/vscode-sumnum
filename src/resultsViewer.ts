938e31f4c4abe0da8855682c33a8acac529456bd
kaskamal <kasra.kamal@gmail.com>
2019-06-30 23:45:36 -0700

    Refactored resultsViewer

--- src/resultsViewer.ts
+++ src/resultsViewer.ts
@@ -1,32 +1,32 @@
-import * as vscode from 'vscode';
+import {Uri, workspace, window, WorkspaceEdit, Position, Range} from 'vscode';
 import { WordCounterController } from './counter/wordCounterController';
 import { WordCounter } from './counter/wordCounter';
 
 export function openResultsFile(wordCounterController: WordCounterController): void {     
-    const newFile = vscode.Uri.parse("untitled:" + 'sum_results_viewer.json');
-    vscode.workspace.openTextDocument(newFile).then(document => {
+    const newFile = Uri.parse("untitled:" + 'sum_results_viewer.json');
+    workspace.openTextDocument(newFile).then(document => {
         const edit = editWorkspace(wordCounterController, newFile);
-        return vscode.workspace.applyEdit(edit).then(success => {
+        return workspace.applyEdit(edit).then(success => {
             if (success) {
-                vscode.window.showTextDocument(document);
+                window.showTextDocument(document);
             } else {
-                vscode.window.showInformationMessage('Error!');
+                window.showInformationMessage('Error!');
             }
         });
     });   
 }
 
-function editWorkspace(wordCounterController: WordCounterController, file: vscode.Uri): vscode.WorkspaceEdit {
+function editWorkspace(wordCounterController: WordCounterController, file: Uri): WorkspaceEdit {
     const wordCounter: WordCounter = wordCounterController.wordCounter;
 
-    const edit = new vscode.WorkspaceEdit();
+    const edit = new WorkspaceEdit();
 
     // Start and end position of file to replace
-    const startFile = new vscode.Position(0, 0);
+    const startFile = new Position(0, 0);
     // Temp solution, need to determine the end line and character of 
     // the file
-    const endFile   = new vscode.Position(100, 100);
+    const endFile   = new Position(100, 100);
 
-    edit.replace(file, new vscode.Range(startFile, endFile), JSON.stringify(wordCounter.wordCount, null, "\t"));
+    edit.replace(file, new Range(startFile, endFile), JSON.stringify(wordCounter.wordCount, null, "\t"));
     return edit;
 } 
